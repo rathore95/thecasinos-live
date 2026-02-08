b9da43df273406172456b7e1ff795dc4a8048963
rathore95 <123986164+rathore95@users.noreply.github.com>
2026-02-09 02:05:10 +0530

    Fix: Use dynamic PORT for Azure deployment + add start script

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
